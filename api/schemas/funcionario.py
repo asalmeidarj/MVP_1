5c60fc15fdf009235d18c8fdb50e46c9213fc515
Alessandro Almeida <asalmeidarj@gmail.com>
2023-07-09 17:27:35 -0300

    feat: add function formatDatetimeISO8601

--- api/schemas/funcionario.py
+++ api/schemas/funcionario.py
@@ -2,6 +2,8 @@ from unicodedata import category
 from pydantic import BaseModel
 from typing import Optional, List
 
+from api.share.utils.date import formatDatetimeISO8601
+
 
 class FuncionarioBodySchema(BaseModel):
     nome: str = "NOME DO FUNCIONARIO"
@@ -12,7 +14,7 @@ class FuncionarioBodySchema(BaseModel):
     complemento: Optional[str] = "NUMERO DO FUNCIONARIO"
     bairro: Optional[str] = "BAIRRO DO FUNCIONARIO"
     cidade: Optional[str] = "CIDADE QUE FUNCIONARIO ESTÁ LOCALIZADO"
-    estado: Optional[str] = "ESTADO QUE FUNCIONARIO ESTÁ LOCALIZAD"
+    estado: Optional[str] = "ESTADO QUE FUNCIONARIO ESTÁ LOCALIZADO"
 
 class FuncionarioBuscaSchema(BaseModel):
     id: Optional[int] = 1
@@ -22,7 +24,7 @@ class FuncionarioViewSchema(BaseModel):
     id: int = 1
     nome: str = "NOME DO FUNCIONARIO"
     cpf: str = "TELEFONE DO FUNCIONARIO"
-    data_insercao: str = "2023-07-09T15:17:41"
+    data_insercao: str = "2023-07-09T15:17:41-03:00"
 
 class FuncionarioDelSchema(BaseModel):
     message: str
@@ -34,7 +36,7 @@ def apresenta_funcionario(funcionario):
         "id": funcionario.id,
         "nome": funcionario.nome,
         "cpf": funcionario.cpf,
-        "data_insercao": funcionario.data_insercao
+        "data_insercao": formatDatetimeISO8601(funcionario.data_insercao)
     }
 
 

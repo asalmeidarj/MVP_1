0ebe57293028b0ef49fe68fa11f4753d1e8c005a
Alessandro Almeida <asalmeidarj@gmail.com>
2023-07-10 01:57:14 -0300

    feat: add formatDatetime

--- api/share/utils/date.py
+++ api/share/utils/date.py
@@ -12,3 +12,12 @@ def formatDatetimeISO8601(date):
     segundos = f"0{segundos}" if segundos < 10 else segundos
 
     return f"{ano}-{mes}-{dia}T{horas}:{minutos}:{segundos}-03:00"
+
+def formatDatetime(date):
+    ano = date.year
+    mes = date.month
+    dia = date.day
+    mes = f"0{mes}" if mes < 10 else mes
+    dia = f"0{dia}" if dia < 10 else dia
+
+    return f"{ano}-{mes}-{dia}"

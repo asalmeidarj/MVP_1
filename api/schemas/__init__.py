eb1251ab9d1b1ae18116cb12ac38cc3864d1f65d
Alessandro Almeida <asalmeidarj@gmail.com>
2023-07-10 01:54:23 -0300

    feat: add schemas ponto

--- api/schemas/__init__.py
+++ api/schemas/__init__.py
@@ -2,4 +2,5 @@ from schemas.empresa import *
 from schemas.cargo import *
 from schemas.cargo_empresa import *
 from schemas.funcionario import *
+from schemas.ponto import *
 from schemas.error import ErrorSchema
